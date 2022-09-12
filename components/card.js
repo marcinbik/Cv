6a74833a8ba2e17ade884065a782fbb7ebe68d7c
Marcin Bikiewicz <marci@DESKTOP-M7T5ID9>
2022-09-12 16:31:35 +0200

    refactor

--- components/card.js
+++ components/card.js
@@ -6,16 +6,16 @@ import Image from "next/image"
 
 export default function Card({ image, title, text}){
     return(
-        <div className="grid xl:w-1/2 mt-2 xl:pt-4 mb-1 max-h-screen">
+        <div className="grid xl:w-1/2   max-h-screen">
 
-        <div className="  border-2 border-[#17181B]  rounded-md mx-1  hover:border-[#9C3848]" >
-            <div className="object-contain relative h-[40vh]   p-4    ">
+        <div className="   m-5 xl:mb-0 rounded-md mx-2  " >
+            <div className="object-contain relative   h-[40vh]       ">
             <Image  src={image} className="rounded-t-md" layout="fill" />
 
             </div>
 
            <div className="bg-[#8F9BAC]  bg-opacity-25  h-[40vh]  shadow-md   rounded-b-md p-2 text-opacity-20" >
-                <div className="mx-3  text-left text-2xl text-white text-opacity-70 p-2 py-6 font-bold">
+                <div className="mx-3  text-center text-4xl text-white text-opacity-70 p-2 py-6 font-bold">
                     {title}
                     
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Heading from '../components/Heading'
 import Card from '../components/card'
 import Footer from '../components/footer'
-import Contact from '../components/Contact'
+import Baner from '../components/baner'
 
 export default function Home() {
   return (
@@ -9,16 +9,7 @@ export default function Home() {
         <div className=' bg-[#17181B] pb-0   mb-0 h-fit'>
           <Heading/>
         <div className="  xl:flex  p-0     pb-0 mb-0  shadows-lg  	]	">
-         <div className=" grid bg-[url('/rudy.jpg')] h-[85vh] bg-cover mx-0 xl:w-5/6 row-span-2 pb-7 mb-0 bg-center  xl:pb-0 " >
-              <div className=' h-1/2 '>
-              </div>
-              
-              <div className='   font-bold text-white text-center  content-end items-center'>
-                <h1 className="  text-7xl shadow-sm ">Marcin Bikiewicz</h1>
-                <p className=' text-2xl shadow-sm text-justyfy '>web developer</p>
-              </div>
-          
-          </div>
+         <Baner/>
           <div className=' xl:inline-flex  flex-grid  h-fit '>
             <Card title={"Strona Reklamowa"} image={"/strona.jpg"} text={"Zbuduje  witrynę zgodną z stadardami dzisiejszych stron. Dopasuje wygląd do branży oraz z tym co sobie zażyczysz. Będziesz chciał coś zmienić? Nie ma problemu. "}/>
             <Card title={"Sklep Internetowy"} image={"/sklep.jpg"} text={" Zaprojektuje nowoczesny sklep internetowy. Zadbam o jego atrakcyjny wygląd. moje sklepy są elastyczne, funkcjonalne i przyjazne dzięki czemu łatwo przyciągniesz klietów."}/>
