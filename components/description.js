e68abae2f9131c6dced4d4e22e8548b37f404078
Marcin Bikiewicz <marci@DESKTOP-M7T5ID9>
2022-09-12 20:38:16 +0200

    new baner

--- components/description.js
+++ components/description.js
@@ -4,7 +4,7 @@ export default function Description(text){
 
         <div className=" bg-black text-2xl md:text-3xl bg-opacity-60 shadow-lg  p-8 md:p-5      h-full" >
         <div className="h-1/5 "></div>
-           <div className="xl:px-80 xl:pl-10">
+           <div className="xl:px-60 ">
             {text.text}
 
            </div>
