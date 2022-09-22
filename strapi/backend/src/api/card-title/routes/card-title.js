9ffdfc372f0be71981bc00f50af1f735e3d999e1
Marcin Bikiewicz <marci@DESKTOP-M7T5ID9>
2022-09-22 17:47:50 +0200

    Initial strapi

---
+++ strapi/backend/src/api/card-title/routes/card-title.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * card-title router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::card-title.card-title');
---
+++ strapi/backend/src/api/card-title/services/card-title.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * card-title service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::card-title.card-title');
