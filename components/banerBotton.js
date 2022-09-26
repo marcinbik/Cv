38838321d4ce34b95ed83ba017d83a0f7dc5f186
Marcin Bikiewicz <marcinbik55@gmail.com>
2022-09-26 11:04:16 +0200

    fix

--- components/banerBotton.js
+++ components/banerBotton.js
@@ -2,7 +2,7 @@ import Description from "./description"
 
 export default function () {
     return(
-        <div className='flex text-white bg-[url("/ligth.jpg")] bg-center text-justyfy'>
+        <div className='flex text-white bg-[url("/ligth.jpg")] p-10 xl:p-40 bg-center '>
           <div className=''>
 
           <Description text={'Jedynie interaktywne rozwiązania prowadzą do sukcesu w biznesie. Dbam o  zadowolenie moich klientów tworząc szerokie spectrum możliwości i technologi.\n\r Projektowanie stron internetowych to dla mnie nie tylko praca, ale także misja, której celem jest budowanie twojego  wizerunku w Internecie.\n\r Stworze dla ciebie skuteczną i profesjonalną witrynę.\n\r Może to być zarówno projekt niewielkiej strony internetowej, jak i mocno rozbudowany serwis.'}/>
